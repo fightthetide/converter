8b1ee73d3f14b35082698770df948c1527d93f4f
Clyde Tedrick <iamclyde@gmail.com>
2021-04-12 06:00:56 -0400

    error scenarios

--- api/controllers/converter.controller.js
+++ api/controllers/converter.controller.js
@@ -1,17 +1,28 @@
 'use strict';
 
 const Decimal = require('decimal.js');
+const { response } = require('express');
 
 const unitLegend = {
   ounce: 29.5735,
   cup: 236.588,
   liter: 1000
-}
+};
 
 exports.convert = function(req, res) {
   const params = req.params;
-  return new Decimal(unitLegend[params.fromUnit])
+  try {
+    const outcome = new Decimal(unitLegend[params.fromUnit])
     .mul(params.qty)
     .div(unitLegend[params.toUnit])
     .toFixed(3);
+
+    res.json({outcome: outcome});
+  } catch(error) {
+    res.status(400);
+    res.json({
+      error: `${error.name}: ${error.message}`,
+      parameters: req.params
+    });
+  }
 };
--- api/controllers/converter.controller.test.js
+++ api/controllers/converter.controller.test.js
@@ -1,14 +1,44 @@
 const converter = require('./converter.controller.js');
+const MockResponse = require('mock-express-response');
+const { expect } = require('@jest/globals');
+require('jest-extended');
 
-const scenarios = [
-    {qty: 1, fromUnit: 'ounce', toUnit: 'cup', outcome: '0.125'},
-    {qty: 1, fromUnit: 'cup', toUnit: 'ounce', outcome: '8.000'},
-    {qty: 1, fromUnit: 'liter', toUnit: 'ounce', outcome: '33.814'}
+// Happy path
+const happyPathScenarios = [
+  { qty: 1, fromUnit: 'ounce', toUnit: 'cup', outcome: '0.125' },
+  { qty: 1, fromUnit: 'cup', toUnit: 'ounce', outcome: '8.000' },
+  { qty: 1, fromUnit: 'liter', toUnit: 'ounce', outcome: '33.814' }
 ];
 
-scenarios.forEach( (scenario) => {
-    let label = `${scenario.qty} ${scenario.fromUnit}(s) to ${scenario.outcome} ${scenario.toUnit}s`;
-    test(label, () => {
-        expect(converter.convert({params: scenario})).toBe(scenario.outcome);
-    });
+happyPathScenarios.forEach((scenario) => {
+  let label = `${scenario.qty} ${scenario.fromUnit}(s) to ${scenario.outcome} ${scenario.toUnit}s`;
+  test(label, () => {
+    let response = new MockResponse();
+    converter.convert({ params: scenario }, response);
+    const payload = response._getJSON();
+    expect(payload.outcome).toBe(scenario.outcome);
+  });
 });
+
+// Unhappy path
+let errorResponse = {
+  error: "Error: [DecimalError] Invalid argument: undefined"
+}
+
+const unhappyScenarios = {
+  'Bad from unit': { qty: 1, fromUnit: 'loud', toUnit: 'cup'},
+  'Bad to unit': { qty: 1, fromUnit: 'cup', toUnit: 'loud' },
+  'Bad quantity': { qty: 'one', fromUnit: 'ounce', toUnit: 'cup' }
+}
+
+for(let scenarioLabel in unhappyScenarios) {
+  let scenario = unhappyScenarios[scenarioLabel];
+  test(scenarioLabel, () => {
+    let response = new MockResponse();
+    converter.convert({ params: scenario }, response);
+    const payload = response._getJSON();
+
+    expect(payload.error).toStartWith('Error: [DecimalError] Invalid argument:');
+    expect(payload.parameters).toStrictEqual(scenario);
+  });
+};
